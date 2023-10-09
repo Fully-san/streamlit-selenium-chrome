ec52ae7805293bb69e248e6ef74bc5d14f47e1af
Fully-san <florent.collot@outlook.com>
2023-10-09 17:53:49 +0200

    plop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,41 @@
-# import streamlit as st
-
-# """
-# ## Web scraping on Streamlit Cloud with Selenium
-
-# [![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
-
-# This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
-
-# Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-# """
-
-# with st.echo():
-#     from selenium import webdriver
-#     from selenium.webdriver.chrome.options import Options
-#     from selenium.webdriver.chrome.service import Service
-#     from webdriver_manager.chrome import ChromeDriverManager
-
-#     @st.experimental_singleton
-#     def get_driver():
-#         return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-#     options = Options()
-#     options.add_argument('--disable-gpu')
-#     options.add_argument('--headless')
-
-#     driver = get_driver()
-#     driver.get("http://example.com")
-
-#     st.code(driver.page_source)
+import streamlit as st
+import time
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
+XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
+TIMEOUT = 20
+
+st.title("Test Selenium")
+st.markdown("You should see some random Football match text below in about 21 seconds")
+
+firefoxOptions = Options()
+firefoxOptions.add_argument("--headless")
+service = Service(GeckoDriverManager().install())
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
+
+try:
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.visibility_of_element_located((By.XPATH, XPATH,))
+    )
+
+except TimeoutException:
+    st.warning("Timed out waiting for page to load")
+    driver.quit()
+
+# time.sleep(10)
+# elements = driver.find_elements_by_xpath(XPATH)
+# st.write([el.text for el in elements])
+# driver.quit()
