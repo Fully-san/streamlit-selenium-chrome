7f0c9d3dff32d3cc4c32824277f33446d8620e35
Fully-san <florent.collot@outlook.com>
2023-10-09 17:45:21 +0200

    plop

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@
 #     st.code(driver.page_source)
 
 from selenium import webdriver
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
