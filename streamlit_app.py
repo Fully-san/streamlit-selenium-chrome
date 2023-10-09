f1ba0b9eced6a68826aefaa78cbf1591a2bf5a24
Fully-san <florent.collot@outlook.com>
2023-10-09 17:29:08 +0200

    plop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,37 @@
-import streamlit as st
+# import streamlit as st
 
-"""
-## Web scraping on Streamlit Cloud with Selenium
+# """
+# ## Web scraping on Streamlit Cloud with Selenium
 
-[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
+# [![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
 
-This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
+# This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
 
-Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
-"""
+# Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+# """
 
-with st.echo():
-    from selenium import webdriver
-    from selenium.webdriver.chrome.options import Options
-    from selenium.webdriver.chrome.service import Service
-    from webdriver_manager.chrome import ChromeDriverManager
+# with st.echo():
+#     from selenium import webdriver
+#     from selenium.webdriver.chrome.options import Options
+#     from selenium.webdriver.chrome.service import Service
+#     from webdriver_manager.chrome import ChromeDriverManager
 
-    @st.experimental_singleton
-    def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+#     @st.experimental_singleton
+#     def get_driver():
+#         return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
+#     options = Options()
+#     options.add_argument('--disable-gpu')
+#     options.add_argument('--headless')
 
-    driver = get_driver()
-    driver.get("http://example.com")
+#     driver = get_driver()
+#     driver.get("http://example.com")
 
-    st.code(driver.page_source)
+#     st.code(driver.page_source)
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromiumService
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+
+driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
