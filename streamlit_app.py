88f645f02941aa4e5cdff4db514f4c7f2c14fd6a
Fully-san <florent.collot@outlook.com>
2023-10-09 17:16:15 +0200

    plop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,30 @@
-from bs4 import BeautifulSoup
-import requests
 import streamlit as st
-import os, sys
 
-url = 'https://marvelsnapzone.com/news/patch-notes/'
+"""
+## Web scraping on Streamlit Cloud with Selenium
 
-page = requests.get(url)
-soup = BeautifulSoup(page.text , 'html.parser')
-st.write(soup.prettify())
+[![Source](https://img.shields.io/badge/View-Source-<COLOR>.svg)](https://github.com/snehankekre/streamlit-selenium-chrome/)
+
+This is a minimal, reproducible example of how to scrape the web with Selenium and Chrome on Streamlit's Community Cloud.
+
+Fork this repo, and edit `/streamlit_app.py` to customize this app to your heart's desire. :heart:
+"""
+
+with st.echo():
+    from selenium import webdriver
+    from selenium.webdriver.chrome.options import Options
+    from selenium.webdriver.chrome.service import Service
+    from webdriver_manager.chrome import ChromeDriverManager
+
+    @st.experimental_singleton
+    def get_driver():
+        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+
+    driver = get_driver()
+    driver.get("http://example.com")
+
+    st.code(driver.page_source)
