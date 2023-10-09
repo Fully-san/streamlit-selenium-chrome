5a65764b699c7c2005aa7980a6c08ee14f7ce408
Fully-san <florent.collot@outlook.com>
2023-10-09 13:49:06 +0200

    plop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
 import streamlit as st
 import os, sys
 
-@st.experimental_singleton
-def installff():
-  os.system('sbase install geckodriver')
-  os.system('ln -s /home/appuser/venv/lib/python3.7/site-packages/seleniumbase/drivers/geckodriver /home/appuser/venv/bin/geckodriver')
+url = 'http://example.com'
 
-_ = installff()
-from selenium import webdriver
-from selenium.webdriver import FirefoxOptions
-opts = FirefoxOptions()
-opts.add_argument("--headless")
-browser = webdriver.Firefox(options=opts)
-
-browser.get('http://example.com')
-st.write(browser.page_source)
+page = requests.get(url)
+soup = BeautifulSoup(page.content , 'html.parser')
+st.write(soup)
