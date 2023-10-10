5decace15a2ea35b8c27d9d503143c761a4226a6
Fully-san <florent.collot@outlook.com>
2023-10-10 12:07:36 +0200

    plop

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-from bs4 import BeautifulSoup
 import streamlit as st
 import time
 
@@ -11,7 +10,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
 
-MARVELSNAPZONE_URL = 'https://marvelsnapzone.com/cards'
+URL = "https://www.unibet.fr/sport/football/europa-league/europa-league-matchs"
 XPATH = "//*[@class='ui-mainview-block eventpath-wrapper']"
 TIMEOUT = 20
 
@@ -19,36 +18,26 @@ st.title("Test Selenium")
 st.markdown("You should see some random Football match text below in about 21 seconds")
 
 firefoxOptions = Options()
-firefoxOptions.add_argument("--headless=new")
-firefoxOptions.add_argument('--disable-dev-shm-usage')
-firefoxOptions.add_argument('--disable-extensions')
-firefoxOptions.add_argument('--disable-gpu')
+firefoxOptions.add_argument("--headless")
 service = Service(GeckoDriverManager().install())
-driver = webdriver.Firefox(service=service, options=firefoxOptions)
-driver.get(MARVELSNAPZONE_URL)
-html = driver.page_source
-st.code(html)
-# soup = BeautifulSoup(html, 'html.parser')
-# links = soup.findAll('a', {'class': 'simple-card'})
-# st.write(links)
+driver = webdriver.Firefox(
+    options=firefoxOptions,
+    service=service,
+)
+driver.get(URL)
 
-# try:
-#     WebDriverWait(driver, TIMEOUT).until(
-#         EC.visibility_of_element_located((By.XPATH, XPATH,))
-#     )
+st.code(driver.page_source)
 
-# except TimeoutException:
-#     st.warning("Timed out waiting for page to load")
-#     driver.quit()
+try:
+    WebDriverWait(driver, TIMEOUT).until(
+        EC.visibility_of_element_located((By.XPATH, XPATH,))
+    )
+
+except TimeoutException:
+    st.warning("Timed out waiting for page to load")
+    driver.quit()
 
 # time.sleep(10)
 # elements = driver.find_elements_by_xpath(XPATH)
 # st.write([el.text for el in elements])
 # driver.quit()
-
-    # browser = webdriver.Chrome(service=chrome_service, options=chrome_options)
-    # browser.get(MARVELSNAPZONE_URL)
-    # html = browser.page_source
-    # soup = BeautifulSoup(html, 'html.parser')
-
-    # links = soup.findAll('a', {'class': 'simple-card'})
